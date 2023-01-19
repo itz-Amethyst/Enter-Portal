76c122f49fdebf885ce905f0747cbed1f2769228
callme-devil <pransermi@gmail.com>
2023-01-19 19:16:21 +0330

    feat: initialized proj

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Japan-Shrine-
+
+## Demo 1
+
+![Japan-Shrine](./src/assets/)
+
+## Demo 2
+
+![Portal-Scene](./src/assets/)
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+
+const root = resolve(__dirname , 'src')
+const outDir = resolve(__dirname , 'dist')
+
+export default defineConfig({
+  root,
+  build: {
+    // Relative to the root
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input:{
+        main: resolve(root ,'index.html'),
+        second: resolve(root ,'Portal-Scene', 'index.html')
+      }
+    }
+  },
+  publicDir: '../static',
+  plugins: [glsl()]
+});
