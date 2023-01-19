5f808389c4503973959638c9644b97a332720de7
callme-devil <pransermi@gmail.com>
2023-01-19 22:23:21 +0330

    feat: added noise to portal shader

--- src/Portal-Scene/shaders/portal/fragment.glsl
+++ src/Portal-Scene/shaders/portal/fragment.glsl
@@ -78,7 +78,15 @@ float cnoise(vec3 P){
 
 void main(){
 
-    float strength = cnoise(vec3(vUv * 5.0 , uTime));
+    // Displace the uv
+    vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0 , uTime * 0.1));
+
+    // Parlin noise
+    float strength = cnoise(vec3(displacedUv * 5.0 , uTime * 0.2));
+
+    // Outer glow
+    float outerGlow = distance(vUv , vec2(0.5)) * 5.0 - 1.4;
+    strength += outerGlow;
 
     gl_FragColor = vec4(strength , strength, strength , 1.0);
 }
